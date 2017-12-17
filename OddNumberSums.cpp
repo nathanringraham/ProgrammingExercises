df003d47a46cea4fe57d7e8512c9ca8ded6459f3
Nathan Ingraham <nathanringraham@gmail.com>
2017-12-17 16:32:49 -0600

    Updated exercise with comments

--- OddNumberSums.cpp
+++ OddNumberSums.cpp
@@ -3,6 +3,8 @@
  * Author: nathan
  *
  * Created on December 17, 2017, 1:07 PM
+ * This program prompts the user for a number.
+ * Once the user has entered a number, its odd product sum is calculated.
  */
 
 #include <cstdlib>
@@ -14,13 +16,17 @@ bool isOdd(int);
 
 int main(int argc, char** argv) {
 
-    cout << "Please enter an integer: ";
+    cout << "Please enter an integer: "; // prompt for number
     int numberToBeUsed;
-    cin >> numberToBeUsed;
+    cin >> numberToBeUsed; //assign number entered to variable
     
+    while (numberToBeUsed <= 0){ // if number is 0 or less than 0, display error and allow user to enter another number.
+    	cout << "Sorry. 0 Is not a valid number. Try again: ";
+    	cin >> numberToBeUsed;
+
+    }
     
-    
-    int totalProducts = calculateProducts(numberToBeUsed);
+    int totalProducts = calculateProducts(numberToBeUsed); //assign the sum of odd products to variable
     
     cout << "Total products: " << totalProducts;
     
@@ -28,14 +34,14 @@ int main(int argc, char** argv) {
 }
 
 
-int calculateProducts(int number){
+int calculateProducts(int number){ //function is passed a number from main, and add its product if and only if it's odd.
     
     int sum = 0;
     
-    for (int x = number; x > 0; x--){
+    for (int x = number; x > 0; x--){ //loop until we reach zero because zero doesn't have a product.
         
         if (isOdd(x)){
-            sum += (x * x);
+            sum += (x * x); //if a number is odd, multiply it by itself and add it to sum.
         }
         
     }
@@ -44,7 +50,7 @@ int calculateProducts(int number){
     
 }
 
-bool isOdd(int number){
+bool isOdd(int number){ //function that determines whether a number is odd or not
     
     if (number % 2 == 0){
         return false;
